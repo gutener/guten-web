b393913bb946e14e98013e4e1d9fe411760ea0a3
rexwong <yongpeng.wong@gmail.com>
2020-06-01 22:32:30 +0800

    @user #channel

--- src/components/medium-editor/Editor.vue
+++ src/components/medium-editor/Editor.vue
@@ -27,7 +27,10 @@
   import InsertEmbed from './libs/InsertEmbed'
   import ListHandler from './libs/ListHandler'
   import ReadMode from './libs/ReadMode'
-  import _ from 'underscore';
+  import _ from 'underscore'
+  import TCMention from './libs/tc-mention'
+  import MentionSearch from './Search.vue'
+  import Vue from 'vue'
 
   export default {
     name: "medium-editor",
@@ -40,7 +43,30 @@
             text: "Write something great!!"
           },
           toolbar: {
-            buttons: ['bold', 'italic', 'quote', 'h1', 'h2', 'h3', 'h4','orderedlist']
+            buttons: ['bold', 'italic', 'quote', 'h1', 'h2', 'h3', 'h4', 'orderedlist']
+          },
+          extensions: {
+            "mention": new TCMention({
+              tagName: "a",
+              renderPanelContent: function (panelEl, currentMentionText, selectMentionCallback) {
+                panelEl.innerHTML = ''
+                const Search = Vue.extend(MentionSearch)
+                const SearchComp = new Search({
+                  propsData: {
+                    mentionText: currentMentionText
+                  },
+                  methods: {}
+                }).$mount()
+
+                const trigger = currentMentionText.substring(0, 1)
+                SearchComp.$on('selectMentionCallback', function (mention) {
+                  mention.text = trigger + mention.text
+                  selectMentionCallback(mention)
+                })
+                panelEl.appendChild(SearchComp.$el)
+              },
+              activeTriggerList: ["#", "@"]
+            })
           }
         },
         hasContent: false
@@ -60,7 +86,8 @@
     components: {
       InsertEmbed,
       ListHandler,
-      ReadMode
+      ReadMode,
+      MentionSearch
     },
     mounted() {
       if (!this.readOnly) {
@@ -69,14 +96,14 @@
     },
     methods: {
       createElm() {
-        this.editor = new MediumEditor(this.$refs.editor, this.editorOptions)
+        this.editor = new MediumEditor(this.$refs.editor, this.editorOptions);
 
         if (this.prefill) {
-          this.hasContent = /<[a-z][\s\S]*>/i.test(this.prefill)
-          this.$refs.editor.focus()
+          this.hasContent = /<[a-z][\s\S]*>/i.test(this.prefill);
+          this.$refs.editor.focus();
         }
 
-        this.editor.subscribe("editableInput", this.triggerChange)
+        this.editor.subscribe("editableInput", this.triggerChange);
       },
       destroyElm() {
         this.editor.destroy()
@@ -107,4 +134,64 @@
   @import "~bulma/css/bulma.css";
   @import "~medium-editor/dist/css/medium-editor.css";
   @import "themes/default.less";
+
+  .medium-editor-mention-panel {
+    font-size: 16px;
+    left: 0;
+    line-height: 1.4;
+    min-width: 100px;
+    position: absolute;
+    text-align: center;
+    top: 0;
+    word-break: break-all;
+    word-wrap: break-word;
+    visibility: hidden;
+    overflow: hidden;
+    z-index: 1000;
+    border: 1px solid rgba(0, 0, 0, 0.15);
+    box-shadow: rgba(101, 119, 134, 0.2) 0px 0px 15px, rgba(101, 119, 134, 0.15) 0px 0px 3px 1px;
+    background-color: white;
+
+    .mention-search__col {
+      -ms-flex-align: stretch;
+      -ms-flex-direction: column;
+      -ms-flex-negative: 0;
+      -ms-flex-preferred-size: auto;
+      -webkit-align-items: stretch;
+      -webkit-box-align: stretch;
+      -webkit-box-direction: normal;
+      -webkit-box-orient: vertical;
+      -webkit-flex-basis: auto;
+      -webkit-flex-direction: column;
+      -webkit-flex-shrink: 0;
+      align-items: stretch;
+      border: 0 solid black;
+      box-sizing: border-box;
+      display: -webkit-box;
+      display: -moz-box;
+      display: -ms-flexbox;
+      display: -webkit-flex;
+      display: flex;
+      flex-basis: auto;
+      flex-direction: column;
+      flex-shrink: 0;
+      margin-bottom: 0px;
+      margin-left: 0px;
+      margin-right: 0px;
+      margin-top: 0px;
+      min-height: 0px;
+      min-width: 0px;
+      padding-bottom: 0px;
+      padding-left: 0px;
+      padding-right: 0px;
+      padding-top: 0px;
+      position: relative;
+      z-index: 0;
+    }
+  }
+
+  .medium-editor-mention-panel-active {
+    visibility: visible;
+    overflow: visible;
+  }
 </style>
---
+++ src/components/medium-editor/Search.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="mention-search__col" v-for="(mention, index) in mentions" :key="mention.id">
+      <button @click="mentionCallback(index)">{{mention.text}}</button>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'MentionSearch',
+    data() {
+      return {
+        trigger:'',
+        mentions: [
+          {
+            id: '232d233',
+            text: '111',
+            url:'https://www.google.com'
+          },
+          {
+            id: '232d2',
+            text: '222',
+            url:'https://www.twitter.com'
+          }
+        ]
+      }
+    },
+    props: {
+      mentionText: {
+        type: String,
+        required: true
+      },
+    },
+    methods: {
+      mentionCallback(index) {
+        const selectedMention=this.mentions[index]
+        this.$emit('selectMentionCallback', selectedMention)
+      }
+    }
+  }
+</script>
